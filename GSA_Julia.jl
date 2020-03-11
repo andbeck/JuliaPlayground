a1e70b1d8740cfa93baeaae890331a8fc648c68f
Andrew <andrew.beckerman@gmail.com>
2020-03-11 22:38:18 +0000

    Learning GSA with tutorial from DiffEq

--- GSA_Julia.jl
+++ GSA_Julia.jl
@@ -1,48 +1,40 @@
-using Plots, DiffEqSensitivity, DifferentialEquations, ParameterizedFunctions
+using Plots, DiffEqSensitivity
+using DifferentialEquations, ParameterizedFunctions
+using Statistics
 
 # plot set
 gr()
 
-# model
 function f(du,u,p,t)
-  du[1] = p[1]*u[1] - p[2]*u[1]*u[2]
-  du[2] = -3*u[2] + u[1]*u[2]
+  du[1] = p[1]*u[1] - p[2]*u[1]*u[2] #prey
+  du[2] = -p[3]*u[2] + p[4]*u[1]*u[2] #predator
 end
+
 u0 = [1.0;1.0]
 tspan = (0.0,10.0)
-p = [1.5,1.0]
+p = [1.5,1.0,3.0,1.0]
 prob = ODEProblem(f,u0,tspan,p)
 t = collect(range(0, stop=10, length=200))
 
-# Morris
+#define and solve problem with defaults
+sol = solve(prob)
 
-m = DiffEqSensitivity.morris_sensitivity(prob,Tsit5(),t,[[1,5],[0.5,5]],[10,10],
-len_trajectory=1500,
-total_num_trajectory=1000,
-num_trajectory=150)
+# time series plot
+plot(sol)
+# phase plane plot
+plot(sol, vars = (1,2))
+
+f1 = function (p)
+  prob1 = remake(prob;p=p)
+  sol = solve(prob1,Tsit5();saveat=t)
+  [mean(sol[1,:]), maximum(sol[2,:])]
+end
+
+
+m = gsa(f1,Morris(total_num_trajectory=1000,num_trajectory=150),[[1,5],[1,5],[1,5],[1,5]])
+m_sobol = gsa(f1,Sobol(),N = 1,[[1,5],[1,5],[1,5],[1,5]])
+
 
 m.means
-m.variances
-
-stdv1 = sqrt.(m.variances[1])
-p = plot(m.means[1]', yerror=stdv1)
-
-s0 = sobol_sensitivity(prob,Tsit5(),t,[[1,5],[0.5,5]],N,0)
-Out[8]: 2-element Array{Array{Float64,2},1}:
- [NaN 0.507831 … 1.00731 1.00436; NaN 1.92336 … 0.732384 0.730945]
- [NaN 0.47214 … 0.676224 0.681525; NaN -1.68656 … 0.879557 0.877603]
-
-s1 = sobol_sensitivity(prob,Tsit5(),t,[[1,5],[0.5,5]],N,1)
-Out[9]: 2-element Array{Array{Float64,2},1}:
- [NaN 0.39537 … 0.341697 0.343645; NaN -2.06101 … 0.10922 0.106976]
- [NaN 0.652815 … 0.00910675 0.00815206; NaN 5.24832 … 0.296978 0.296639]
-
-s2 = sobol_sensitivity(prob,Tsit5(),t,[[1,5],[0.5,5]],N,2)
-Out[10]: 1-element Array{Array{Float64,2},1}:
- [NaN -0.0596478 … 0.652303 0.657847; NaN -1.84504 … 0.645139 0.620036]
-
-p1 = bar(["a","b"],[s0[1][end-2],s0[2][end-2]],color=[:red,:blue],title="Total Order Indices at t=9.949748743718592",legend=false)
-p2 = bar(["a","b"],[s1[1][end-2],s1[2][end-2]],color=[:red,:blue],title="First Order Indices at t=9.949748743718592",legend=false)
-p3 = bar(["a","b"],[s0[1][3],s0[2][3]],color=[:red,:blue],title="Total Order Indices at t=0.05025125628140704",legend=false)
-p4 = bar(["a","b"],[s1[1][3],s1[2][3]],color=[:red,:blue],title="First Order Indices at t=0.05025125628140704",legend=false)
-plo = plot(p1,p2,p3,p4,layout=(4,1),size=(600,500))
+scatter(m.means[1,:], m.variances[1,:],series_annotations=[:a,:b,:c,:d],color=:gray)
+scatter(m.means[2,:], m.variances[2,:],series_annotations=[:a,:b,:c,:d],color=:gray)

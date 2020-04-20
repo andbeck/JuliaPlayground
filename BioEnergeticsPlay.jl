bd04f506e4047df1ca62a19b138864b7a35080a4
Andrew Beckerman <andrew.beckerman@gmail.com>
2020-04-20 23:11:15 +0100

    recreating and fixing MEE sims for Julia 1.0

--- BioEnergeticsPlay.jl
+++ BioEnergeticsPlay.jl
@@ -6,4 +6,23 @@ using BioEnergeticFoodWebs
 # gr
 gr()
 
-# basic simulation with BEFW
+# basic simulation with BEFW from MEE paper
+# generate random food web
+
+A = nichemodel(20,0.15, toltype = :abs)
+
+for a in range(0.92, stop = 1.08, length = 3) # change from MEE with J 1.0
+    for K in exp10.(range(-1,1,length = 9)) # changed from MEE paper with J 1.0 + typo in paper
+        p = model_parameters(A, α = a, K = K, productivity = :competitive)
+
+        # We start each simulation with
+        # random biomasses in ]0;1[
+        bm = rand(size(A, 1))
+
+        # Andfinally,wesimulate.
+        out = simulate(p, bm, start = 0, stop = 2000) # omit method from MEE
+
+        # And measure the output
+        diversity = foodweb_evenness(out, last = 1000) # omit eps() from MEE
+    end
+end

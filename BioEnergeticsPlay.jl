b67d6076c9c609f0c9c0937d6ace8d083e70e6ae
Andrew Beckerman <andrew.beckerman@gmail.com>
2020-04-21 22:23:56 +0100

    working on loop and collecting of MEE paper

--- BioEnergeticsPlay.jl
+++ BioEnergeticsPlay.jl
@@ -1,7 +1,6 @@
 ## Working with BioEnergeticFoodWebs
 
-using Plots
-using BioEnergeticFoodWebs
+using Plots, BioEnergeticFoodWebs
 
 # gr
 gr()
@@ -10,10 +9,17 @@ gr()
 # generate random food web
 
 A = nichemodel(20,0.15, toltype = :abs)
+a = range(0.92, stop = 1.08, length = 3)
+K = exp10.(range(-1,1,length = 9))
 
-for a in range(0.92, stop = 1.08, length = 3) # change from MEE with J 1.0
-    for K in exp10.(range(-1,1,length = 9)) # changed from MEE paper with J 1.0 + typo in paper
-        p = model_parameters(A, α = a, K = K, productivity = :competitive)
+diversity = zeros((9,3))
+
+for j in 1:length(a)  # change from MEE with J 1.0
+    for i in 1:length(K)  # changed from MEE paper with J 1.0 + typo in paper
+
+        global(diversity)
+
+        p = model_parameters(A, α = a[j], K = K[i], productivity = :competitive)
 
         # We start each simulation with
         # random biomasses in ]0;1[
@@ -23,6 +29,8 @@ for a in range(0.92, stop = 1.08, length = 3) # change from MEE with J 1.0
         out = simulate(p, bm, start = 0, stop = 2000) # omit method from MEE
 
         # And measure the output
-        diversity = foodweb_evenness(out, last = 1000) # omit eps() from MEE
+        diversity[i,j] = foodweb_evenness(out, last = 1000) # omit eps() from MEE
     end
 end
+
+plot(diversity[1], K)

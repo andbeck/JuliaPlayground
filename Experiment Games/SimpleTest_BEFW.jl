e2e6b15e678935b065ab43404d3e51ba16c12d28
Andrew Beckerman <andrew.beckerman@gmail.com>
2021-03-17 11:45:31 +0000

    saved simple test...

--- Experiment Games/SimpleTest_BEFW.jl
+++ Experiment Games/SimpleTest_BEFW.jl
@@ -1,7 +1,9 @@
-using BioEnergeticFoodWebs EcologicalNetworks
+using BioEnergeticFoodWebs, EcologicalNetworks, Random, Pkg
+
+#Pkg.status()
 
 Random.seed!(21)
 A = Int.(adjacency(EcologicalNetworks.nichemodel(20,0.15)))
-p = model_parameters(A, h= 1.0, K = 1.0)
+p = model_parameters(A, h= 1.0, K = [1.0])
 bm = rand(size(A,1))
 out = simulate(p, bm, start = 0, stop = 2000)

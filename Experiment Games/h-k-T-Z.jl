20f2f3c7f8c83eeed93d05fdf590ae75c4bde4d2
Andrew Beckerman <andrew.beckerman@gmail.com>
2021-03-24 08:58:18 +0000

    h-k-t-z update with temperature scaling to actually work

--- Experiment Games/h-k-T-Z.jl
+++ Experiment Games/h-k-T-Z.jl
@@ -1,31 +1,41 @@
 using BioEnergeticFoodWebs, Plots, Random, Pkg
 using DataFrames, CSV
 
+## Utilities for Scaling and Temperature ##
+include("utils.jl")
+
+## The Starting Matrix ## 
+# need to generate reps....
 Random.seed!(21)
 A = BioEnergeticFoodWebs.nichemodel(20,0.15)
 bm = rand(size(A,1))
 
+## The Grid of Values ##
+
 Z = [1.0,10.0,100.0]
 h = [1.0,1.2, 2.0]
 K = exp10.(range(-1,1,length = 10))
-T = [293.15-10, 293.15-5, 293.15, 293.15-5, 293.15+10]
+T = [293.15-10, 293.15-5, 293.15, 293.15+5, 293.15+10]
 
-(3*3*10*5)
-
-p = model_parameters(A, Z = Z[1], h = h[1], K = [K[1]], T = T[2])
-out = simulate(p, bm, start = 0, stop = 2000, interval_tkeep = 1) 
+## The Collector ##
 
 outdf = DataFrame(Z = [], h = [], K = [], T = [],
                     persistence = [], biomass = [], stability = [])
 
+#= 
+This is the master loop
+Use functional_response=:classical and ScaleRates! and don't forget we are in real time
+So that stop is big and interval_tkeep to keep it fast.
+=#
 
 for i in 1:length(Z)
     for j in 1:length(h)
         for k in 1:length(K)
             for l in 1:length(T)
                 println(i, j, k, l)
-                p = model_parameters(A, Z = Z[i], h = h[j], K = [K[k]], T = T[l])
-                out = simulate(p, bm, start = 0, stop = 2000, interval_tkeep = 1)
+                p = model_parameters(A, Z = Z[i], h = h[j], K = [K[k]], T = T[l], rewire_method = :none, functional_response = :classical)
+                ScaleRates!(p)
+                out = simulate(p, bm, start = 0, stop = 2000000000, interval_tkeep = 10000)
                 persistence = species_persistence(out, last = 1000)
                 biomass = population_biomass(out, last = 1000)
                 stability = population_stability(out, last = 1000)
@@ -40,8 +50,12 @@ end
 
 outdf
 
-plot(outdf[!,"T"], outdf[!,"persistence"])
-
 CSV.write("outdf.csv", outdf)
 
 
+(3*3*10*5)
+
+p = model_parameters(A, Z = Z[1], h = h[1], K = [K[1]], T = T[1], functional_response = :classical)
+ScaleRates!(p)
+out = simulate(p, bm, start = 0, stop = 2000000000, interval_tkeep = 10000) 
+plot(out[:B], legend = false)
--- Experiment Games/h-k_exp.R
+++ Experiment Games/h-k_exp.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(patchwork)
 
 df <- read_csv("h-k_exp.csv")
-df2 <- read_csv("outdf.csv")
 
 sumDat_div <- df %>% 
   group_by(h,K) %>% 
@@ -26,9 +25,31 @@ p3 <- ggplot(sumDat_div, aes(x = K, y = meanStability, colour = factor(h)))+
 
 p1+p2+p3
 
-###
-ggplot(df2, aes(x = K, y = persistence, colour = factor(h),
+### h-k-t-z
+
+df2 <- read_csv("outdf.csv")
+glimpse(df2)
+df2 %>% distinct(`T`)
+p1 <- ggplot(df2, aes(x = K, y = persistence,
+                colour = factor(h),
+                shape = factor(h),
                 group = h))+
   geom_point()+
   geom_line()+
-  facet_grid(`T` ~ Z)
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p2 <- ggplot(df2, aes(x = K, y = stability,
+                      colour = factor(h),
+                      shape = factor(h),
+                      group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p1+p2
+
+ggplot(df2, aes(x = persistence, y = stability, colour = factor(h)))+
+  geom_point()+
+  facet_grid(K ~ `T`+Z)

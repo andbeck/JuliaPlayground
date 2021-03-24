59ca13364ee6a5c361e3ca1ad5bd22bd7470a346
Andrew Beckerman <andrew.beckerman@gmail.com>
2021-03-24 00:31:42 +0000

    Eva's fix, andrew fixes h-k-t-z, utils.jl from eva and R plotting of h-k-t-z

--- Experiment Games/h-k-T-Z.jl
+++ Experiment Games/h-k-T-Z.jl
@@ -1,15 +1,16 @@
-using BioEnergeticFoodWebs, EcologicalNetworks, Plots, Random, Pkg
-using DataFrames
+using BioEnergeticFoodWebs, Plots, Random, Pkg
+using DataFrames, CSV
 
 Random.seed!(21)
 A = BioEnergeticFoodWebs.nichemodel(20,0.15)
 bm = rand(size(A,1))
 
-Z = [1.0,10.0]
-h = [1,2]
-K = [1.0, 10.0]
-T = [273.15-10, 273.15, 273.15+10]
+Z = [1.0,10.0,100.0]
+h = [1.0,1.2, 2.0]
+K = exp10.(range(-1,1,length = 10))
+T = [293.15-10, 293.15-5, 293.15, 293.15-5, 293.15+10]
 
+(3*3*10*5)
 
 p = model_parameters(A, Z = Z[1], h = h[1], K = [K[1]], T = T[2])
 out = simulate(p, bm, start = 0, stop = 2000, interval_tkeep = 1) 
@@ -37,7 +38,10 @@ for i in 1:length(Z)
 end
 
 
+outdf
 
+plot(outdf[!,"T"], outdf[!,"persistence"])
 
+CSV.write("outdf.csv", outdf)
 
 

05e015a116b08e225b1b09f96b1db135f84d3e7b
Andrew Beckerman <andrew.beckerman@gmail.com>
2021-03-24 13:50:48 +0000

    add rewire to h-k-t-z sim and update R plotting

--- Experiment Games/h-k-T-Z.jl
+++ Experiment Games/h-k-T-Z.jl
@@ -19,8 +19,8 @@ T = [293.15-10, 293.15-5, 293.15, 293.15+5, 293.15+10]
 
 ## The Collector ##
 
-outdf = DataFrame(Z = [], h = [], K = [], T = [],
-                    persistence = [], biomass = [], stability = [])
+outdf_no_rewire = DataFrame(Z = [], h = [], K = [], T = [], persistence = [], biomass = [], stability = [], diversity = [])
+outdf_rewire = DataFrame(Z = [], h = [], K = [], T = [], persistence = [], biomass = [], stability = [], diversity = [])
 
 #= 
 This is the master loop
@@ -33,29 +33,46 @@ for i in 1:length(Z)
         for k in 1:length(K)
             for l in 1:length(T)
                 println(i, j, k, l)
-                p = model_parameters(A, Z = Z[i], h = h[j], K = [K[k]], T = T[l], rewire_method = :none, functional_response = :classical)
-                ScaleRates!(p)
-                out = simulate(p, bm, start = 0, stop = 2000000000, interval_tkeep = 10000)
-                persistence = species_persistence(out, last = 1000)
-                biomass = population_biomass(out, last = 1000)
-                stability = population_stability(out, last = 1000)
-                diversity = foodweb_evenness(out, last = 1000)
+                p_nr = model_parameters(A, Z = Z[i], h = h[j], K = [K[k]], T = T[l], rewire_method = :none, functional_response = :classical)
+                p_r = model_parameters(A, Z = Z[i], h = h[j], K = [K[k]], T = T[l], rewire_method = :ADBM, functional_response = :classical)
+                ScaleRates!(p_nr)
+                ScaleRates!(p_r)
+                # 10 years
+                out_nr = simulate(p_nr, bm, start = 0, stop = 60*60*24*365*10, interval_tkeep = 10000)
+                out_r = simulate(p_r, bm, start = 0, stop = 60*60*24*365*10, interval_tkeep = 10000)
+                
+                persistence_nr = species_persistence(out_nr, last = 1000)
+                biomass_nr = population_biomass(out_nr, last = 1000)
+                stability_nr = population_stability(out_nr, last = 1000)
+                diversity_nr = foodweb_evenness(out_nr, last = 1000)
 
-                push!(outdf, [Z[i], h[j], K[k], T[l], persistence, biomass, stability])
+                persistence_r = species_persistence(out_r, last = 1000)
+                biomass_r = population_biomass(out_r, last = 1000)
+                stability_r = population_stability(out_r, last = 1000)
+                diversity_r = foodweb_evenness(out_r, last = 1000)
+
+                push!(outdf_no_rewire, [Z[i], h[j], K[k], T[l], persistence_nr, biomass_nr, stability_nr, diversity_nr])
+                push!(outdf_rewire, [Z[i], h[j], K[k], T[l], persistence_r, biomass_r, stability_r, diversity_r])
             end
         end
     end
 end
 
 
-outdf
+outdf_no_rewire
+outdf_rewire
 
-CSV.write("outdf.csv", outdf)
+CSV.write("outdf_nr.csv", outdf_no_rewire)
+CSV.write("outdf_r.csv", outdf_rewire)
+
 
 
 (3*3*10*5)
 
 p = model_parameters(A, Z = Z[1], h = h[1], K = [K[1]], T = T[1], functional_response = :classical)
 ScaleRates!(p)
-out = simulate(p, bm, start = 0, stop = 2000000000, interval_tkeep = 10000) 
+p[:r] # should be a set of smalls and 0.
+
+# simulate for x years 60*60*24*365*x in seconds, keep relatively few
+out = simulate(p, bm, start = 0, stop = 60*60*24*365*10, interval_tkeep = 10000) 
 plot(out[:B], legend = false)
--- Experiment Games/h-k_exp.R
+++ Experiment Games/h-k_exp.R
@@ -1,6 +1,76 @@
 library(tidyverse)
 library(patchwork)
 
+### h-k-t-z - APB with Eva - what is re-wiring doing.
+
+df_r <- read_csv("outdf_r.csv")
+df_nr <- read_csv("outdf_nr.csv")
+
+glimpse(df_r)
+
+p1 <- ggplot(df_nr, aes(x = K, y = persistence,
+                colour = factor(h),
+                shape = factor(h),
+                group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p2 <- ggplot(df_nr, aes(x = K, y = stability,
+                      colour = factor(h),
+                      shape = factor(h),
+                      group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p3 <- ggplot(df_nr, aes(x = K, y = diversity,
+                        colour = factor(h),
+                        shape = factor(h),
+                        group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p4 <- ggplot(df_r, aes(x = K, y = persistence,
+                        colour = factor(h),
+                        shape = factor(h),
+                        group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p5 <- ggplot(df_r, aes(x = K, y = stability,
+                        colour = factor(h),
+                        shape = factor(h),
+                        group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+p6 <- ggplot(df_r, aes(x = K, y = diversity,
+                        colour = factor(h),
+                        shape = factor(h),
+                        group = h))+
+  geom_point()+
+  geom_line()+
+  facet_grid(`T` ~ Z)+
+  scale_x_log10()
+
+(p1+p2+p3)/(p4+p5+p6)
+
+ggplot(df_r, aes(x = persistence, y = stability, colour = factor(h)))+
+  geom_point()+
+  facet_grid(Z ~ `T`)
+
+
+
+# Masters mini project ----------
 df <- read_csv("h-k_exp.csv")
 
 sumDat_div <- df %>% 
@@ -10,7 +80,7 @@ sumDat_div <- df %>%
     meanBiomass = mean(biomass, na.rm = TRUE),
     meanStability = mean(stability, na.rm = TRUE),
   )
-  
+
 p1 <- ggplot(sumDat_div, aes(x = K, y = meanDiv, colour = factor(h)))+
   geom_line()+
   scale_x_log10()
@@ -24,32 +94,3 @@ p3 <- ggplot(sumDat_div, aes(x = K, y = meanStability, colour = factor(h)))+
   scale_x_log10()
 
 p1+p2+p3
-
-### h-k-t-z
-
-df2 <- read_csv("outdf.csv")
-glimpse(df2)
-df2 %>% distinct(`T`)
-p1 <- ggplot(df2, aes(x = K, y = persistence,
-                colour = factor(h),
-                shape = factor(h),
-                group = h))+
-  geom_point()+
-  geom_line()+
-  facet_grid(`T` ~ Z)+
-  scale_x_log10()
-
-p2 <- ggplot(df2, aes(x = K, y = stability,
-                      colour = factor(h),
-                      shape = factor(h),
-                      group = h))+
-  geom_point()+
-  geom_line()+
-  facet_grid(`T` ~ Z)+
-  scale_x_log10()
-
-p1+p2
-
-ggplot(df2, aes(x = persistence, y = stability, colour = factor(h)))+
-  geom_point()+
-  facet_grid(K ~ `T`+Z)
